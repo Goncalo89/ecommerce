bfa821e8b97195f6b930c526199fbf3ab3802ad0
Gonçalo <goncalopintoalves@gmail.com>
2021-10-18 16:52:03 +0100

    Admin Login

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '53a92fabfc5bc7a955283fd831a44418f029f365',
         'name' => 'goncalo89/ecommerce',
         'dev' => true,
     ),
     'versions' => array(
         'goncalo89/ecommerce' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '53a92fabfc5bc7a955283fd831a44418f029f365',
             'dev_requirement' => false,
         ),
         'phpmailer/phpmailer' => array(
